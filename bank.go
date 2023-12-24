64cd1fa162416746b88c045458feadec6a8cf67e
Saufi <msaufi2325@gmail.com>
2023-12-24 20:36:02 +0900

    loop - replacing for with switch

--- bank.go
+++ bank.go
@@ -6,32 +6,60 @@ func main() {
 	var accountBalance = 1000.0
 
 	fmt.Println("Welcome to Go bank")
-	fmt.Println("What do you want to do?: ")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+	for {
+		fmt.Println("What do you want to do?: ")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	// wantsCheckBalance := choice == 1
-	if choice == 1 {
-		fmt.Printf("Your balance is %0.2f\n", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-		accountBalance += +depositAmount
-		fmt.Printf("Balance updated! New amount: %0.2f\n", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Withdrawal amount: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-		accountBalance -= withdrawAmount
-		fmt.Printf("Balance updated! New amount: %0.2f\n", accountBalance)
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			fmt.Printf("Your balance is %0.2f\n", accountBalance)
+
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				// return
+				continue
+			}
+
+			accountBalance += +depositAmount
+			fmt.Printf("Balance updated! New amount: %0.2f\n", accountBalance)
+
+		case 3:
+			fmt.Print("Withdrawal amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				// return
+				continue
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have. ")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Printf("Balance updated! New amount: %0.2f\n", accountBalance)
+
+		default:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank")
+			return
+		}
+
 	}
-
-	fmt.Printf("Your choice: %v\n", choice)
 }
